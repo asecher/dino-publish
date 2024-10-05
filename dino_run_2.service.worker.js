af7a4eebb3b1c2395e2b770ded6e2cbd2e1f972d
Alexis SÉCHER <asecher@2jprocess.com>
2024-10-05 17:49:49 +0200

    kiiiit

--- dino_run_2.service.worker.js
+++ dino_run_2.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728128786|3039190204';
+const CACHE_VERSION = '1728143335|1640236014';
 /** @type {string} */
 const CACHE_PREFIX = 'dino_run_2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
